9da1d5ac2fa808de1384b0bd5978444a7e66d53b
Kareem El-Wishahy <kareem.elwishahy@gmail.com>
2022-11-09 12:37:42 -0800

    Initial Commit

---
+++ heads_up_prototype/android/app/src/main/kotlin/com/example/heads_up_prototype/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.heads_up_prototype
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ heads_up_prototype/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import './screens/home.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Heads Up',
+      home: Home(),
+    );
+  }
+}
